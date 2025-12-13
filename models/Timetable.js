d1d60bd8ad072a208842de3faaa7724e22ab0065
Atif Shahzad <zarkosportswear@gmail.com>
2025-12-13 14:00:02 +0500

    uplading

--- models/Timetable.js
+++ models/Timetable.js
@@ -20,7 +20,7 @@ const timetableSchema = new mongoose.Schema({
             },
             teacher: {
                 type: mongoose.Schema.Types.ObjectId,
-                ref: 'Teacher',
+                ref: 'User',
                 required: true
             },
             startTime: {
